de34cb0d74883000c85a38933843c383d11c5698
claire.kelley-23 <clairekel23@gmail.com>
2023-04-06 12:52:47 -0400

    Updated EntriesChartTracker and Added ParentEntries

---
+++ src/Components/EntriesChartTracker/EntriesChartTracker.jsx
@@ -0,0 +1,26 @@
+import React, { useState, useEffect } from 'react';
+import { Chart } from "react-google-charts";
+
+const EntriesChartTracker = (props) => {
+
+const [chartData, setChartData] = useState([]);
+
+    useEffect(() => {
+        let tempChartData = props.parentEntries.map(entry => {
+            return [entry.date, entry.weight];
+        });
+        setChartData(tempChartData);
+    })
+
+    return (
+        <Chart
+        chartType="LineChart"
+        data={[["Age", "Weight"], ...chartData]}
+        width="100%"
+        height="400px"
+        legendToggle
+        />
+      );
+}
+ 
+export default EntriesChartTracker;
