9f1b2bd754b19e19b4a6396c115cb52dabac4e35
claire.kelley-23 <clairekel23@gmail.com>
2023-04-06 12:15:37 -0400

    Updated Components File and AddEntry Submit Button

--- src/Components/DisplayEntries/DisplayEntries.jsx
+++ src/Components/DisplayEntries/DisplayEntries.jsx
@@ -1,3 +1,5 @@
+
+
 const DisplayEntries = (props) => {
     return (
         <table>
@@ -9,7 +11,7 @@ const DisplayEntries = (props) => {
           </tr>
         </thead>
         <tbody>
-          {entries.map((entry, index) => {
+          {props.parentEntries.map((entry, index) => {
             return (
               <tr>
                 <td>{index + 1}</td>
